435771682122f8bf85ec889913e0f380aa3725f8
Tyler Weldon <tylerweldon94@gmail.com>
2020-09-04 13:36:45 -0400

    interpolate aws credentials

--- main.tf
+++ main.tf
@@ -15,3 +15,9 @@ resource "vultr_object_storage" "unit" {
   object_storage_cluster_id = data.vultr_region.region.id
   label = var.storage_unit_label
 }
+
+provider "aws" {
+  region = "us-east-1"
+  access_key = vultr_object_storage.unit.s3_access_key
+  secret_key = vultr_object_storage.unit.s3_secret_key
+}
