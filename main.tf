4d283ada93c350d631de42cccd54da7104de2d55
Tyler Weldon <tylerweldon94@gmail.com>
2020-09-04 17:16:51 -0400

    maybe force path address?

--- main.tf
+++ main.tf
@@ -28,6 +28,7 @@ provider "aws" {
   skip_requesting_account_id = true
   skip_get_ec2_platforms = true
   skip_metadata_api_check = true
+  s3_force_path_style = true
 
   endpoints {
     s3 = "ewr1.vultrobjects.com"
