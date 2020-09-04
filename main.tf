c99b93345400c6dd98fa3e32a5dac7d5cee18047
Tyler Weldon <tylerweldon94@gmail.com>
2020-09-04 15:29:20 -0400

    another skip

--- main.tf
+++ main.tf
@@ -28,6 +28,7 @@ provider "aws" {
   skip_region_validation = true
   skip_requesting_account_id = true
   skip_get_ec2_platforms = true
+  skip_metadata_api_check = true
   endpoints {
     s3 = "ewr1.vultrobjects.com"
   }
