8dbda11c2dc2c5b96e4f2d959532b2c85eddd2fa
Tyler Weldon <tylerweldon94@gmail.com>
2020-09-04 16:06:30 -0400

    testing public acl

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "project_state" {
   bucket = "${uuid()}-${var.storage_unit_label}"
-  acl   = "private"
+  acl   = "public"
 
   lifecycle_rule {
     enabled = false
